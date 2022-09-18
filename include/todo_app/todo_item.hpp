9b0e2b7903fa792bb2e323ce67c4bb9a130dd7fc
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-17 21:23:16 -0600

    Add ToDoItem class
    Added ToDoItem class that has the following attributes:
        unique_id
        title
        created_at
        completed_at
        task_status
    
    Unique_id is a string representing unique hex values.
    Title will act as the title of the task.
    Create_at will contain a date in which the task was created.
    Completed_at will contain the date in which the task has been marked complete.
    Task status will represent whether the task is in progress or complete.
    
    There are getters for all attributes which are public except
    completed_at as access to that attribute is limited and should be only
    shown when the task state is complete.
    
    There is also support for changing the status from in progress to
    complete which will update the completed_at attribute.
    
    Added method to return a string representing the status of the
    task_status enum.
    
    Support for the << operator so that the class can be used with
    std::cout.

---
+++ include/todo_app/todo_item.hpp
@@ -0,0 +1,93 @@
+#ifndef FV_TODO_ITEM
+#define FV_TODO_ITEM
+
+#include <ostream>
+#include <string>
+
+#include "date.hpp"
+
+namespace fv_todo {
+	/*
+	 * @brief Designates two states of a task, in progress and complete
+	 */
+	enum class Status {
+		IN_PROGRESS,
+		COMPLETE
+	};
+	
+	class ToDoItem {
+		private:
+			std::string _unique_id;
+			std::string _title;
+			Date _created_at;
+			Date _completed_at;
+			Status _task_status;
+
+			Date completed_at() const;
+		public:
+			/*
+			 * @brief Parameterized constructor that takes a title and 
+			 * 	automatically generates an id, will initialize the 
+			 * 	_created_at attribute to the current timestamp
+			 */
+			ToDoItem(std::string title);
+
+			/*
+			 * @brief Getter for _unique_id attribute
+			 *
+			 * @return String containing pseudo-unique id
+			 */
+			std::string uid() const;
+
+			/*
+			 * @brief Getter for _title attribute
+			 *
+			 * @return String containing the title of the task
+			 */
+			std::string title() const;
+
+			/*
+			 * @brief Getter for _created_at attribute
+			 *
+			 * @return A date class containing the task creation date
+			 */
+			Date created_at() const;
+
+			/*
+			 * @brief Getter for _task_status attribute
+			 *
+			 * @return Status enum detailing if the task is in-progress or
+			 * 	complete
+			 */
+			Status status() const;
+
+			/*
+			 * @brief Will mark the task as complete and change the 
+			 * 	_task_status from IN_PROGRESS to COMPLETE. The _completed_at
+			 * 	attribute will also update to mark when the task is completed.
+			 */ 
+			void mark_complete();
+
+			/*
+			 * @brief Will take the enum and convert it to a string to
+			 * 	represent the current state
+			 *
+			 * @return String representing the current status of the enum
+			 */
+			std::string status_to_str() const;
+
+			/*
+			 * @brief Adds additional functionality to the << operator, so
+			 *  it can be printed with std::cout
+			 *
+			 * @param out An output stream (most likely cout) to write to
+			 * @param tdi The ToDoItem class to print the attributes of
+			 *
+			 * @return The output stream after the class attributes have
+			 * 	been written to
+			 */
+			friend std::ostream& operator<<(
+					std::ostream& out, const ToDoItem& tdi);
+	};
+}
+#endif // FV_TODO_ITEM
---
+++ src/todo_item_impl.cpp
@@ -0,0 +1,46 @@
+#include "../include/todo_app/todo_item.hpp"
+#include "../include/todo_app/uid_generator.hpp"
+
+#include <ostream>
+
+namespace fv_todo {
+	ToDoItem::ToDoItem(std::string title) : _created_at(), _completed_at(),
+		_task_status{Status::IN_PROGRESS}, _title{title} {
+		_unique_id = generate_uid();
+
+	}
+	std::string ToDoItem::uid() const {
+		return _unique_id;
+	}
+
+	std::string ToDoItem::title() const {
+		return _title;
+	}
+
+	Date ToDoItem::created_at() const {
+		return _created_at;
+	}
+
+	Date ToDoItem::completed_at() const {
+		return _completed_at;
+	}
+
+	Status ToDoItem::status() const {
+		return _task_status;
+	}
+
+	void ToDoItem::mark_complete() {
+		_task_status = Status::COMPLETE;
+		_completed_at.update();
+	}
+
+	std::ostream& operator<<(std::ostream& out, const ToDoItem& tdi) {
+		out << "Task: " << tdi.title() << '\n' << "ID: " << tdi.uid() << '\n';
+		out << "Status: " << tdi.status_to_str() << "\nCreated: ";
+		out << tdi.created_at() << '\n';
+		if (tdi.status() == Status::COMPLETE) {
+			out << "Completed: " << tdi.completed_at() << '\n';
+		}
+		return out;
+	}
+}
