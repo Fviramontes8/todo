6c07b3028874acbfc4faf116cc222b558c843bcf
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-19 23:36:27 -0600

    Update main app
    Application will now print the contents of the database

--- app/app.cpp
+++ app/app.cpp
@@ -7,11 +7,15 @@ int main() {
 	fv_todo::Date today;
 	std::cout << "Today's date: " << today << "\n\n";
 
-	fv_todo::ToDoItem init_task("Show this instance");
+	fv_todo::ToDoItem init_task("Print this task");
 	std::cout << init_task << '\n';
 
 	fv_todo::SQLiteDB db;
 	db.add_task(init_task);
+	db.print_database();
+//	db.read_task(init_task.uid());
+//	db.delete_task(init_task.uid());
+//	db.print_database();
 
 	return 0;
 }
