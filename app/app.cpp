5bfb04e2c8da2d9d4ab1faef942246636c886d43
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-21 18:06:39 -0600

    Update main app
    Updated application driver code to use the ToDoApp class

--- app/app.cpp
+++ app/app.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 
-#include "../include/todo_app/todo_item.hpp"
-#include "../include/todo_app/sqlite_database.hpp"
+#include "../include/todo_app/core_app.hpp"
 
 int main() {
-	fv_todo::Date today;
-	std::cout << "Today's date: " << today << "\n\n";
-
-	fv_todo::ToDoItem init_task("Print this task");
-	std::cout << init_task << '\n';
-
-	fv_todo::SQLiteDB db;
-	db.add_task(init_task);
-	db.print_database();
-//	db.read_task(init_task.uid());
-//	db.delete_task(init_task.uid());
-//	db.print_database();
-
+	 fv_todo::ToDoApp app;
+	 while(1) {
+	 	app.prompt();
+	 	app.process_user_input();
+	 }
 	return 0;
 }
