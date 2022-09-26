7b2744e1801a9f6cce301270e062c7ae591e2ab2
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-25 22:51:18 -0600

    Update app
    Application now starts a webpage server using crow. There are three
    things the user can do:
        Visit a homepage
        Visit a todo task page to add a task to local database
        Visit a page to indicate success or failure of adding task

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
+#include <filesystem>
+#include <sstream>
+#include "crow.h"
 
 #include "../include/todo_app/core_app.hpp"
 
 int main() {
-	 fv_todo::ToDoApp app;
-	 while(1) {
-	 	app.prompt();
-	 	app.process_user_input();
-	 }
+	 fv_todo::ToDoApp todo_app;
+	 // CLI code:
+//	 while(1) {
+//	 	todo_app.prompt();
+//	 	todo_app.process_user_input();
+//	 }
+
+	crow::SimpleApp app;
+	CROW_ROUTE(app, "/")(
+		[]() {
+			return fv_todo::homepage();
+		}
+	);
+
+	CROW_ROUTE(app, "/add_task")(
+		[]() {
+			return fv_todo::add_task_page();
+		}
+	);
+
+	CROW_ROUTE(app, "/adding_task")(
+		[&todo_app](const crow::request& req) {
+			if (req.url_params.get("title") != nullptr) {
+				std::string title{req.url_params.get("title")};
+				todo_app.push_task(title);
+				todo_app.print_tasks();
+				return fv_todo::add_task_success();
+			}
+			return fv_todo::add_task_failure();
+		}
+	);
+
+	app.port(8080).multithreaded().run();
+
 	return 0;
 }
